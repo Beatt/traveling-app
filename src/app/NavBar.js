6ecc381325c5af49c869c707aa230cc286ad7170
Gabriel Jiménez <ggabrieljimenezmartinez@gmail.com>
2021-11-27 19:24:54 -0600

    config sass

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,5 +1,18 @@
 import * as React from "react"
+import "./styles.scss"
 
-const NavBar = () => <div>NavBar</div>
+const NavBar = () => (
+  <div className="navbar">
+    <div className="pure-g">
+      <div className="pure-u-1 pure-u-md-4-24" />
+      <div className="pure-u-1 pure-u-md-12-24">
+        <p>Traveling like a pro</p>
+      </div>
+      <div className="pure-u-1 pure-u-md-4-24">
+        <p>icono</p>
+      </div>
+    </div>
+  </div>
+)
 
 export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./styles.scss"
 import React from "react"
 import ReactDOM from "react-dom"
 import reportWebVitals from "./reportWebVitals"
